9b0eebfcb7089cdea73a2cd4e94b0ae2710cbf01
abhi6262 <89373320+abhi6262@users.noreply.github.com>
2021-09-12 14:01:10 -0700

    Adding incsat code

---
+++ incsat.h
@@ -0,0 +1,42 @@
+/**CFile****************************************************************
+
+  FileName    [incsat.h]
+
+  SystemName  [ABC: Logic synthesis and verification system.]
+
+  PackageName [Iterative Layering - Incremental SAT Package.]
+
+  Synopsis    [External declarations.]
+
+  Author      [Abhishek Sharma]
+  
+***********************************************************************/
+ 
+#ifndef INC_SAT_H
+#define INC_SAT_H
+
+////////////////////////////////////////////////////////////////////////
+///                          INCLUDES                                ///
+////////////////////////////////////////////////////////////////////////
+
+#include "base/main/main.h"
+#include "epfl/eglobal/eglobal.h"
+#include "sat/cnf/cnf.h"
+#include "aig/aig/aig.h"
+#include "base/abc/abc.h"
+#include "sat/bsat/satSolver.h"
+#include "sat/bsat/satStore.h"
+
+////////////////////////////////////////////////////////////////////////
+///                         PARAMETERS                               ///
+////////////////////////////////////////////////////////////////////////
+
+ABC_NAMESPACE_HEADER_START
+        
+ABC_NAMESPACE_HEADER_END
+
+#endif
+        
+////////////////////////////////////////////////////////////////////////
+///                       END OF FILE                                ///
+////////////////////////////////////////////////////////////////////////
